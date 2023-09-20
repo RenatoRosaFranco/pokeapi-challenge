2575c70c12e0a6624fb4ba82e03a9d23f60ded62
Renato Rosa Franco <renato_ny@live.com>
2023-09-20 19:45:38 -0300

    fix: fixing cache application time.

--- app/controllers/http/cacheable.rb
+++ app/controllers/http/cacheable.rb
@@ -2,7 +2,7 @@
 
 module Http
   module Cacheable
-    CACHE_EXPIRATION_TIME = 1.hour
+    CACHE_EXPIRATION_TIME = 1.minute
 
     def generate_cache(suffix, name)
       "#{suffix}_#{name}"
@@ -14,6 +14,7 @@ module Http
 
     def write_cache(cache_key, serialized_object, expiration = CACHE_EXPIRATION_TIME)
       Rails.cache.write(cache_key, serialized_object, expires_in: expiration)
+      Rails.logger.info("Created cache at #{Time.now}") if !Rails.env.production?
     end
   end
 end
